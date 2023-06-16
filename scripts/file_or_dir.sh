7000efb29bee49780cd51243aa226677f52ca80c
Kai Schell <kai.schell@oscert.eu>
2023-06-16 11:53:27 +0200

    test if file exists

--- scripts/file_or_dir.sh
+++ scripts/file_or_dir.sh
@@ -8,5 +8,25 @@
 # && -> logisches UND, rechter Teil wird nur ausgeführt wenn linker erfolgreich
 # || -> logisches ODER, rechter Teil wird nur ausgeführt wenn linker NICHT erfolgreich
 
-test -f $1 && echo "$1 ist eine reguläre Datei"
-test -d $1 && echo "$1 ist ein Verzeichnis"
+# Prüfung auf Existenz
+if ! test -e $1; then
+  echo "$1: existiert nicht."
+  exit 1
+fi
+
+# Berechtigungen aus ls Listing ausschneiden
+berechtigungen=$(ls -ld $1 | cut -d' ' -f1)
+
+# Prüfung auf Datei
+if test -f $1; then	
+  echo "$1: ist eine reguläre Datei" 
+  file $1 
+  exit 0
+fi
+
+# Prüfung auf Verzeichnis
+if test -d $1; then
+  echo "$1: ist ein Verzeichnis"
+  echo "$1: hat folgende Berechtigungen: $berechtigungen"
+  exit 0
+fi
