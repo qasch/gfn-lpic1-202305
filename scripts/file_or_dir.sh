7f39d9dfe86a9586c9a22d275148c41baa0d322e
Kai Schell <kai.schell@oscert.eu>
2023-06-16 14:18:13 +0200

    reg ex and string comparison

--- scripts/file_or_dir.sh
+++ scripts/file_or_dir.sh
@@ -5,10 +5,26 @@
 # test -f -> datei
 # test -d -> verzeichnis
 
+set +x
+
 # Prüfung auf Existenz
 if [ ! -e $1 ]; then
   echo "$1: existiert nicht."
   #exit 1
+
+  read -p "Möchtest du die Datei $1 erstellen? [j/n]" eingabe
+  echo eingabe: $eingabe
+
+  # Datei erstellen
+  #if [ "$eingabe" = "J" ] || [ $eingabe == "j" ] ; then
+  #if [[ "$eingabe" =~ [Jj]a? ]]; then
+  readonly accepted_answers='Ja ja J j jawoll Jawoll Klar klar Klaro klaro yes Yes y Y'
+  if [[ $accepted_answers == *$eingabe* ]]; then
+    touch $1
+    echo Datei $1 wurde erstellt.
+  else 
+    echo 'Na dann nicht...'
+  fi
 else
   # Berechtigungen aus ls Listing ausschneiden
   berechtigungen=$(ls -ld $1 | cut -d' ' -f1)
