c26ed78f36103c397f2ec6c6483c4a53de6d7c29
Kai Schell <kai.schell@oscert.eu>
2023-06-16 13:24:31 +0200

    add else, remove exit

--- scripts/file_or_dir.sh
+++ scripts/file_or_dir.sh
@@ -8,22 +8,23 @@
 # Prüfung auf Existenz
 if [ ! -e $1 ]; then
   echo "$1: existiert nicht."
-  exit 1
+  #exit 1
+else
+  # Berechtigungen aus ls Listing ausschneiden
+  berechtigungen=$(ls -ld $1 | cut -d' ' -f1)
+  
+  # Prüfung auf Datei
+  if [ -f $1 ]; then	
+    echo "$1: ist eine reguläre Datei" 
+    file $1 
+    exit 0
+  fi
+  
+  # Prüfung auf Verzeichnis
+  if [ -d $1 ]; then
+    echo "$1: ist ein Verzeichnis"
+    echo "$1: hat folgende Berechtigungen: $berechtigungen"
+    exit 0
+  fi
 fi
 
-# Berechtigungen aus ls Listing ausschneiden
-berechtigungen=$(ls -ld $1 | cut -d' ' -f1)
-
-# Prüfung auf Datei
-if [ -f $1 ]; then	
-  echo "$1: ist eine reguläre Datei" 
-  file $1 
-  exit 0
-fi
-
-# Prüfung auf Verzeichnis
-if [ -d $1 ]; then
-  echo "$1: ist ein Verzeichnis"
-  echo "$1: hat folgende Berechtigungen: $berechtigungen"
-  exit 0
-fi
