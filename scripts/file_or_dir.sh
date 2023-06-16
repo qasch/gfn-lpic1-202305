06e36c7ec3ce8b52ccfcebbc24278338a37b428a
Kai Schell <kai.schell@oscert.eu>
2023-06-16 11:56:58 +0200

    replace test with []

--- scripts/file_or_dir.sh
+++ scripts/file_or_dir.sh
@@ -5,11 +5,8 @@
 # test -f -> datei
 # test -d -> verzeichnis
 
-# && -> logisches UND, rechter Teil wird nur ausgeführt wenn linker erfolgreich
-# || -> logisches ODER, rechter Teil wird nur ausgeführt wenn linker NICHT erfolgreich
-
 # Prüfung auf Existenz
-if ! test -e $1; then
+if [ ! -e $1 ]; then
   echo "$1: existiert nicht."
   exit 1
 fi
@@ -18,14 +15,14 @@ fi
 berechtigungen=$(ls -ld $1 | cut -d' ' -f1)
 
 # Prüfung auf Datei
-if test -f $1; then	
+if [ -f $1 ]; then	
   echo "$1: ist eine reguläre Datei" 
   file $1 
   exit 0
 fi
 
 # Prüfung auf Verzeichnis
-if test -d $1; then
+if [ -d $1 ]; then
   echo "$1: ist ein Verzeichnis"
   echo "$1: hat folgende Berechtigungen: $berechtigungen"
   exit 0
